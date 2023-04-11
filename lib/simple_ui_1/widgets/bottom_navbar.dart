b9190f379e561abe8e7e1dba11457ce59721697e
koplakzs <atom.go.76@gmail.com>
2023-04-12 01:08:27 +0700

    implementasi DotNavigationBar Success

--- lib/simple_ui_1/widgets/bottom_navbar.dart
+++ lib/simple_ui_1/widgets/bottom_navbar.dart
@@ -1,3 +1,4 @@
+import 'package:dot_navigation_bar/dot_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_ui/simple_ui_1/theme/my_theme.dart';
 
@@ -15,28 +16,39 @@ class BottomNavigation extends StatefulWidget {
 class _BottomNavigationState extends State<BottomNavigation> {
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
+    return DotNavigationBar(
       backgroundColor: MyTheme.backgroundBlue,
-      fixedColor: MyTheme.backgroundWhite,
-      items: const [
-        BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home_filled,
-              size: 25,
-            ),
-            label: 'Home'),
-        BottomNavigationBarItem(
-            icon: Icon(
-              Icons.person_2_rounded,
-              size: 25,
-            ),
-            label: 'Profile'),
-      ],
+      selectedItemColor: MyTheme.backgroundWhite,
+      unselectedItemColor: MyTheme.backgroundWhite,
       currentIndex: widget.currentIndex,
       onTap: widget.onItemTapped,
-      type: BottomNavigationBarType.fixed,
-      showUnselectedLabels: false,
-      selectedLabelStyle: const TextStyle(fontSize: 14),
+      items: [
+        DotNavigationBarItem(
+            icon: const CircleAvatar(
+          radius: 13,
+          backgroundColor: MyTheme.backgroundWhite,
+          child: Icon(
+            Icons.home,
+            size: 20,
+            color: MyTheme.backgroundBlue,
+          ),
+        )),
+        DotNavigationBarItem(
+            icon: const CircleAvatar(
+          radius: 13,
+          backgroundColor: MyTheme.backgroundWhite,
+          child: Icon(
+            Icons.person,
+            size: 20,
+            color: MyTheme.backgroundBlue,
+          ),
+        )),
+      ],
+      marginR: const EdgeInsets.all(0),
+      borderRadius: 0,
+      paddingR: const EdgeInsets.symmetric(horizontal: 40, vertical: 5),
+      enablePaddingAnimation: false,
+      itemPadding: const EdgeInsets.symmetric(horizontal: 18, vertical: 5),
     );
   }
 }
